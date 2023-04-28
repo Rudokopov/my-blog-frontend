02df7dd30fe005f5b23818d326d4b70e0f98c4f3
Pavel Uzuniants <holabeda@gmail.com>
2023-04-28 17:04:12 +0300

    feat: Сделана мобильная версия

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -13,6 +13,7 @@ export const Home = () => {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.auth.data);
   const { posts, tags } = useSelector((state) => state.posts);
+  const [width, setWidth] = useState();
 
   const isPostsLoading = posts.status === "loading";
   const isTagsLoading = tags.status === "loading";
@@ -20,6 +21,12 @@ export const Home = () => {
   useEffect(() => {
     dispatch(fetchPosts());
     dispatch(fetchTags());
+  }, []);
+
+  // experience with mob vers.
+  useEffect(() => {
+    const windowInnerWidth = window.innerWidth;
+    setWidth(windowInnerWidth);
   }, []);
 
   return (
@@ -32,61 +39,120 @@ export const Home = () => {
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
-      <Grid container spacing={4}>
-        <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
-            isPostsLoading ? (
-              <Post key={index} isLoading={true} />
-            ) : (
-              <Post
-                _id={obj._id}
-                key={obj._id}
-                title={obj.title}
-                imageUrl={
-                  obj.imageUrl
-                    ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
-                    : ""
-                }
-                user={{
-                  avatarUrl: obj.owner.avatarUrl,
-                  fullName: obj.owner.name,
-                }}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={
-                  userData === null
-                    ? false
-                    : userData.userData?._id === obj.owner._id // Ломает прилу при обновлении страницы
-                }
-              />
-            )
-          )}
-        </Grid>
-        <Grid xs={4} item>
-          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
-          <CommentsBlock
-            items={[
-              {
-                user: {
-                  fullName: "Вася Пупкин",
-                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+
+      {width > 800 ? (
+        <Grid container spacing={4}>
+          <Grid xs={8} item>
+            {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+              isPostsLoading ? (
+                <Post key={index} isLoading={true} />
+              ) : (
+                <Post
+                  _id={obj._id}
+                  key={obj._id}
+                  title={obj.title}
+                  imageUrl={
+                    obj.imageUrl
+                      ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                      : ""
+                  }
+                  user={{
+                    avatarUrl: obj.owner.avatarUrl,
+                    fullName: obj.owner.name,
+                  }}
+                  createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  commentsCount={3}
+                  tags={obj.tags}
+                  isEditable={
+                    userData === null
+                      ? false
+                      : userData.userData?._id === obj.owner._id // Ломает прилу при обновлении страницы
+                  }
+                />
+              )
+            )}
+          </Grid>
+          <Grid xs={4} item>
+            <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+            <CommentsBlock
+              items={[
+                {
+                  user: {
+                    fullName: "Вася Пупкин",
+                    avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                  },
+                  text: "Это тестовый комментарий",
                 },
-                text: "Это тестовый комментарий",
-              },
-              {
-                user: {
-                  fullName: "Иван Иванов",
-                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                {
+                  user: {
+                    fullName: "Иван Иванов",
+                    avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                  },
+                  text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
                 },
-                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
-              },
-            ]}
-            isLoading={false}
-          />
+              ]}
+              isLoading={false}
+            />
+          </Grid>
         </Grid>
-      </Grid>
+      ) : (
+        <Grid container spacing={4} flexDirection={"column"}>
+          <Grid xs={12} item>
+            {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+              isPostsLoading ? (
+                <Post key={index} isLoading={true} />
+              ) : (
+                <Post
+                  _id={obj._id}
+                  key={obj._id}
+                  title={obj.title}
+                  imageUrl={
+                    obj.imageUrl
+                      ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                      : ""
+                  }
+                  user={{
+                    avatarUrl: obj.owner.avatarUrl,
+                    fullName: obj.owner.name,
+                  }}
+                  createdAt={obj.createdAt}
+                  viewsCount={obj.viewsCount}
+                  commentsCount={3}
+                  tags={obj.tags}
+                  isEditable={
+                    userData === null
+                      ? false
+                      : userData.userData?._id === obj.owner._id // Ломает прилу при обновлении страницы
+                  }
+                />
+              )
+            )}
+          </Grid>
+          <Grid xs={12} item>
+            <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+            <CommentsBlock
+              items={[
+                {
+                  user: {
+                    fullName: "Вася Пупкин",
+                    avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                  },
+                  text: "Это тестовый комментарий",
+                },
+                {
+                  user: {
+                    fullName: "Иван Иванов",
+                    avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                  },
+                  text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+                },
+              ]}
+              isLoading={false}
+            />
+          </Grid>
+        </Grid>
+      )}
     </>
   );
 };
